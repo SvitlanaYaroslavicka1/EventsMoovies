55985e8c7a579c144961cdcaf8e3f51772e90919
Your Name <you@example.com>
2022-01-13 14:01:28 +0200

    add modal content and styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,9 +7,52 @@ const Modal = ({ closeModal, movie }) => {
   return createPortal(
     <div className="modal">
       <div className="modal-main">
-        <h1>{movie.id}</h1>
-        <button type="button" onClick={() => closeModal(null)}>
-          Close
+        <div className="modal-main__poster-block">
+          <img
+            className="modal-main__poster-image"
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={`${movie.title} Poster`}
+          />
+        </div>
+        <div className="modal-main__inner-container">
+          <div className="modal-main__film-info__description">
+            <h2 className="film-info__title">{movie.title}</h2>
+            <ul className="film-info__table">
+              <li className="film-info__table-item">
+                <p className="film-info__table-line-title">Vote / Votes</p>
+                <span className="film-info__vote">{movie.vote_average}</span>/
+                <span clas="film-info__votes">{movie.vote_count}</span>
+              </li>
+              <li className="film-info__table-item">
+                <p className="film-info__table-line-title">Popularity</p>
+                <span className="film-info__popularity">
+                  {movie.popularity}
+                </span>
+              </li>
+              <li className="film-info__table-item">
+                <p clasName="film-info__table-line-title">Original Title</p>
+                <span className="film-info__original-title">
+                  {movie.original_title}
+                </span>
+              </li>
+              <li className="film-info__table-item">
+                <p clasName="film-info__table-line-title">Release Date</p>
+                <span className="film-info__popularity">
+                  {movie.release_date}
+                </span>
+              </li>
+            </ul>
+            <h3 className="film-info__about-title">About</h3>
+            <p className="film-info__about-text">{movie.overview}</p>
+          </div>
+        </div>
+
+        <button
+          className="modal-main__button-close"
+          type="button"
+          onClick={() => closeModal(null)}
+        >
+          X
         </button>
       </div>
     </div>,
--- varibles.js
+++
@@ -1,9 +0,0 @@
-export const titles = {
-  logoHeaderTitle: 'Events movies',
-  homePageTitle: 'Popular movies',
-  myMoviesHeaderTitle: 'My Watchlist',
-  noMoviesTitle: 'No movies in your list! Add some!',
-  watchedButtonTitle: 'Add to Watched',
-  addToWatchListButtonTitle: 'Add to your movies',
-  watchedHeaderTitle: 'Watched Movies',
-};
